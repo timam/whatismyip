ba1dcfb2a014493854649cb6c9e33b7355f1e151
Timam <mail@timam.io>
2021-05-20 09:04:14 +0600

    added spinner

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/janeczku/go-spinner"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func main()  {
+func WhatIsMyIP()  {
 	resp, err := http.Get("https://whatismyipapi.herokuapp.com/")
 	if err != nil{
 		fmt.Println("Something Went Wrong!!", err)
@@ -17,7 +18,14 @@ func main()  {
 			log.Fatal(err)
 		}else {
 			bodyString := string(body)
-			fmt.Println("Your public IP address is:", bodyString)
+			fmt.Println("\nYour public IP address is:", bodyString)
 		}
 	}
+}
+
+func main()  {
+	s := spinner.StartNew("Waiting for server response...")
+	s.SetCharset([]string{"🌑 ", "🌒 ", "🌓 ", "🌔 ", "🌕 ", "🌖 ", "🌗 ", "🌘 "})
+	WhatIsMyIP()
+	s.Stop()
 }
